4c9f0a759a17ba811f105a1a51b7917d3aa348ba
NovicePaso <135040767+NovicePaso@users.noreply.github.com>
2023-07-26 21:27:37 -0700

    Initialize html, css and js files Created files, html boilerplate and linked css and js.  Created skeleton functionality: - grid of divs (with visual) - function createGrid - event listener "hover" to add new class - fill background color to new class

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function createGrid(gridLength){
+    for (let i = 0; i < gridLength; i++){
+        const gridColumn = document.createElement("div");
+        gridColumn.classList.add("grid-column");
+        document.querySelector(".grid-container").appendChild(gridColumn);
+        for(let j = 0; j < gridLength; j++){
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("grid-element");
+            gridColumn.appendChild(gridElement);
+    }
+}
+}
+
+createGrid(16);
+
+// set up "hover" effect with addEventListener for when a mouse enters a "grid-element".  use parameter "once"
+var fillGrid = document.getElementsByClassName("grid-element");
+
+
+for(let i = 0; i < fillGrid.length; i++){
+    fillGrid[i].addEventListener("mouseover", function(){fillGrid[i].classList.add("filled-element")});
+}
+
+for(let i = 0; i< fillGrid.length; i++){
+    fillGrid[i].addEventListener("click", function(){fillGrid[i].classList.add("colored-element")});
+}
