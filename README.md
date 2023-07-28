d2707786d1a4fddf1f5a520fc533888746d34b46
NovicePaso <135040767+NovicePaso@users.noreply.github.com>
2023-07-28 09:15:36 -0700

    Create button to allow user input for gridLength Create button to allow user to input gridLength. Must be a positive number (max 100 squares).  Also added a feature that removes a filled element by clicking on it.

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Goal 1:
 Create an interactive Etch-a-Sketch simulation
 - allows users to "sketch" on a given grid of squares
 - provide users the option of determining the number of squares per side in the grid (up to 100 max)
+
+Lessons Learned:
+- DOM manipulation: querySelector, querySelectorAll, getElementsByClassName, classList.add/remove, createElement, appendChild
+- Event Listeners: mouseover and click events
+
+Goal 2:
+Extra credit
+Transform the behavior of a square when interacting with the mouse by introducing a series of modifications.
+
+1. Rather than a simple color change from black to white, each interaction should randomize the square’s RGB value entirely.
+2. Additionally, implement a progressive darkening effect where each interaction adds 10% more black or color to the square. The objective is to achieve a completely black square only after ten interactions.
--- javascript.js
+++ javascript.js
@@ -11,18 +11,40 @@ function createGrid(gridLength){
 }
 }
 
-createGrid(16);
+// user selects size input of grid
+document.querySelector("#selector").addEventListener("click", function(){
+var input = prompt("How many number of squares per side?", 16);
+if (input > 100){
+    alert("Maximum input is 100");
+    input = 0;
+}
+else if (input < 0){
+    alert("Must be a positive integer");
+    input = 0;
+}
+else if (isNaN(input)){
+        alert("Please input a number");
+        input = 0;
+}
+
+var gridColumns = document.querySelectorAll(".grid-column");
+for (let i = 0; i < gridColumns.length; i++){
+    gridColumns[i].remove(".grid-column");
+}
+createGrid(input);
 
 // set up "hover" effect with addEventListener for when a mouse enters a "grid-element".  use parameter "once"
 var fillGrid = document.getElementsByClassName("grid-element");
 
-
 for(let i = 0; i < fillGrid.length; i++){
-    fillGrid[i].addEventListener("mouseover", function(){fillGrid[i].classList.add("filled-element")});
+    fillGrid[i].addEventListener("mouseover", function(){
+        fillGrid[i].classList.add("filled-element")
+    });
 }
+// on click event, removes filled element
+for(let i = 0; i< fillGrid.length; i++){
+    fillGrid[i].addEventListener("click", function(){fillGrid[i].classList.remove("filled-element")});
+}
+})
 
 
-// on click event, adds class to colored-element
-/* for(let i = 0; i< fillGrid.length; i++){
-    fillGrid[i].addEventListener("click", function(){fillGrid[i].classList.add("colored-element")});
-} */
